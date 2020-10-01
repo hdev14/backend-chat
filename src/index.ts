ed6564f256be101f3aacf39552ce10dad73bb1b4
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-01 13:07:36 -0300

    feat: create sendMessage

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,25 @@ const getChatId = function (url: string | undefined): string | null {
   return chatId
 }
 
+const sendMessage = function (chatId: string | null, client: Websocket, message: Websocket.Data) {
+  const clientChatId = getChatId(client.url)
+  if (clientChatId === chatId) {
+    client.send(message)
+  }
+}
+
 wsServer.on('connection', function (ws, request) {
-  const chatId = getChatId(request.url)
-
+  ws.url = request.url || ''
+  const chatId = getChatId(ws.url)
   ws.on('message', function (message) {
-
+    wsServer.clients.forEach(function (client) {
+      if (client.readyState === Websocket.OPEN && client !== ws) {
+        client.send(message)
+        sendMessage(chatId, client, message)
+      }
+    })
   })
 
-  console.log(`Chat ID -> ${getChatId(request.url)}`)
+  console.log(`Chat ID -> ${chatId}`)
   console.log('Connection is open...')
 })
