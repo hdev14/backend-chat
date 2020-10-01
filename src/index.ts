b0658b6261ec28f851f57e178cf6727dc3d4b6ab
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-01 12:07:50 -0300

    feat: create getChatId function

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import Websocket from 'ws'
 
 const wsServer = new Websocket.Server({ port: 4141 })
 
-wsServer.on('connection', function (ws) {
+const getChatId = function (url: string | undefined): string | null {
+  const params = url?.split('/')[1]
+  const chatId = (new URLSearchParams(params)).get('id')
+  return chatId
+}
+
+wsServer.on('connection', function (ws, request) {
+  const chatId = getChatId(request.url)
+
   ws.on('message', function (message) {
-    console.log(`Received: ${message}`)
+
   })
 
+  console.log(`Chat ID -> ${getChatId(request.url)}`)
   console.log('Connection is open...')
 })
