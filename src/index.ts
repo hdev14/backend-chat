a348be123574da1912a0a3db505aa1711b340904
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-01 11:06:00 -0300

    feat: create simple ws server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hellow')
+import Websocket from 'ws'
+
+const wsServer = new Websocket.Server({ port: 4141 })
+
+wsServer.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    console.log(`Received: ${message}`)
+  })
+
+  console.log('Connection is open...')
+})
