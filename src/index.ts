1d7c65037be6e323ca91b9a3290cca911c4dbff0
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-02 10:35:39 -0300

    feat: add close connection message

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,54 @@
-import Websocket from 'ws'
+import WebSocket from 'ws'
 
-const wsServer = new Websocket.Server({ port: 4141 })
-
-const getChatId = function (url: string | undefined): string | null {
-  const params = url?.split('/')[1]
-  const chatId = (new URLSearchParams(params)).get('id')
-  return chatId
+type MessageData = {
+  message: string
+  date: Date
 }
 
-const sendMessage = function (chatId: string | null, client: Websocket, message: Websocket.Data) {
-  const clientChatId = getChatId(client.url)
-  if (clientChatId === chatId) {
-    client.send(message)
+let connectionCount = 0
+
+const wsServer = new WebSocket.Server({ port: 4141 })
+
+const getUrlParam = function (url: string, field: string): string | null {
+  const params = url?.split('/')[1]
+  return (new URLSearchParams(params)).get(field)
+}
+
+const isSameChatId = function (currentUrl: string, clientUrl: string): boolean {
+  return getUrlParam(currentUrl, 'id') === getUrlParam(clientUrl, 'id')
+}
+
+const sendMessage = function (current: WebSocket, client: WebSocket, messageData: MessageData) {
+  if (client !== current &&
+    client.readyState === WebSocket.OPEN &&
+    isSameChatId(current.url, client.url)) {
+    const data = JSON.stringify({
+      name: getUrlParam(client.url, 'name'),
+      ...messageData
+    })
+    client.send(data)
   }
 }
 
 wsServer.on('connection', function (ws, request) {
+  connectionCount++
   ws.url = request.url || ''
-  const chatId = getChatId(ws.url)
-  ws.on('message', function (message) {
+
+  ws.on('message', function (data) {
+    const messageData = <MessageData>JSON.parse(<string>data)
     wsServer.clients.forEach(function (client) {
-      if (client.readyState === Websocket.OPEN && client !== ws) {
-        client.send(message)
-        sendMessage(chatId, client, message)
-      }
+      sendMessage(ws, client, messageData)
     })
   })
 
-  console.log(`Chat ID -> ${chatId}`)
-  console.log('Connection is open...')
+  ws.on('close', function () {
+    wsServer.clients.forEach(function (client) {
+      sendMessage(ws, client, {
+        message: 'desconectado',
+        date: new Date()
+      })
+    })
+  })
+
+  console.log(`${connectionCount} clients connected`)
 })
