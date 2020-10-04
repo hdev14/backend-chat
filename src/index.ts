e61b8f5fbc75b58ce953f2e8991e9a248c24c954
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-04 13:11:15 -0300

    refactor: change arrow func to regular func

--- src/index.ts
+++ src/index.ts
@@ -16,16 +16,16 @@ let connectionCount = 0
 
 const wsServer = new WebSocket.Server({ port: 4141 })
 
-const getUrlParam = function (url: string, field: string): string | null {
+function getUrlParam (url: string, field: string): string | null {
   const params = url?.split('/')[1]
   return (new URLSearchParams(params)).get(field)
 }
 
-const isSameChatId = function (currentUrl: string, clientUrl: string): boolean {
+function isSameChatId (currentUrl: string, clientUrl: string): boolean {
   return getUrlParam(currentUrl, 'id') === getUrlParam(clientUrl, 'id')
 }
 
-const sendMessage = function (current: WebSocket, client: WebSocket, messageData: MessageData): void {
+function sendMessage (current: WebSocket, client: WebSocket, messageData: MessageData): void {
   if (client !== current &&
     client.readyState === WebSocket.OPEN &&
     isSameChatId(current.url, client.url)) {
